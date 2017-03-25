f0faf3dc837468f7beebaf8f65385d3cb91eb070
Justin Anastos <justin.anastos@gmail.com>
2017-03-25 08:44:19 -0400

    Allow random newlines in import-destructuring-spacing

--- tests/lib/rules/import-destructuring-spacing.js
+++ tests/lib/rules/import-destructuring-spacing.js
@@ -23,7 +23,7 @@ ruleTester.run('import-destructuring-spacing', rule, {
 
   valid: [
     {
-      code: "import { a, b, c, d, e, f, g, h } from 'somewhere'",
+      code: "import { a , b, c, d, e, f, g, h } from 'somewhere'",
       parserOptions,
     },
     {
@@ -77,14 +77,89 @@ ruleTester.run('import-destructuring-spacing', rule, {
         minProperties: 3,
       }],
     },
+    {
+      code: `
+        import type { FetchLayoutPageDiffActionCreator }
+          // test
+          from '../../../actions/layout-page-action/fetch-page-diff-action';
+      `,
+      parser: 'babel-eslint',
+      parserOptions,
+      options: [{
+        enforceIndentation: false,
+      }],
+    },
+    {
+      code: `
+        import type { FetchLayoutPageDiffActionCreator }
+          from '../../../actions/layout-page-action/fetch-page-diff-action';
+      `,
+      parser: 'babel-eslint',
+      parserOptions,
+      options: [{
+        enforceIndentation: false,
+      }],
+    },
+    {
+      code: "import type { FetchLayoutPageDiffActionCreator } from '../../../actions/layout-page-action/fetch-page-diff-action'",
+      parser: 'babel-eslint',
+      parserOptions,
+      options: [{
+        enforceIndentation: false,
+      }],
+    },
+    {
+      code: `
+        import FetchLayoutPageDiffActionCreator
+          from '../../../actions/layout-page-action/fetch-page-diff-action';
+      `,
+      parserOptions,
+      options: [{
+        enforceIndentation: false,
+      }],
+    },
   ],
 
   invalid: [
     {
+      code: `
+        import type { a }
+          from 'somewhere';
+      `,
+      errors: [
+        'line break missing between bracket and \'a\'',
+        'line break missing between \'a\' and closing bracket',
+      ],
+      parser: 'babel-eslint',
+      parserOptions,
+      options: [{
+        enforceIndentation: false,
+        minProperties: 1,
+      }],
+    },
+    {
       code: "import {a, b, c} from 'somewhere'",
       errors: [
-        "missing line break between 'a' and 'b'",
-        "missing line break between 'b' and 'c'",
+        'line break missing between bracket and \'a\'',
+        'missing line break between \'a\' and \'b\'',
+        'missing line break between \'b\' and \'c\'',
+        'line break missing between \'c\' and closing bracket',
+      ],
+      parserOptions,
+      options: [{
+        enforceIndentation: false,
+        minProperties: 3,
+      }],
+    },
+    {
+      code: `
+        import {a,
+          b,
+        c} from 'somewhere'
+      `,
+      errors: [
+        'line break missing between bracket and \'a\'',
+        'line break missing between \'c\' and closing bracket',
       ],
       parserOptions,
       options: [{
@@ -95,13 +170,15 @@ ruleTester.run('import-destructuring-spacing', rule, {
     {
       code: "import { a, b, c, d, e, f, g, h } from 'somewhere'",
       errors: [
-        "missing line break between 'a' and 'b'",
-        "missing line break between 'b' and 'c'",
-        "missing line break between 'c' and 'd'",
-        "missing line break between 'd' and 'e'",
-        "missing line break between 'e' and 'f'",
-        "missing line break between 'f' and 'g'",
-        "missing line break between 'g' and 'h'",
+        'line break missing between bracket and \'a\'',
+        'missing line break between \'a\' and \'b\'',
+        'missing line break between \'b\' and \'c\'',
+        'missing line break between \'c\' and \'d\'',
+        'missing line break between \'d\' and \'e\'',
+        'missing line break between \'e\' and \'f\'',
+        'missing line break between \'f\' and \'g\'',
+        'missing line break between \'g\' and \'h\'',
+        'line break missing between \'h\' and closing bracket',
       ],
       parserOptions,
       options: [{
