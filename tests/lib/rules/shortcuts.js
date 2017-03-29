e34885904073d093eb60613a59f1705e54cd7d92
Justin Anastos <justin.anastos@gmail.com>
2017-03-29 11:02:48 -0400

    Add more tests and fixers to shortcuts

--- tests/lib/rules/shortcuts.js
+++ tests/lib/rules/shortcuts.js
@@ -37,48 +37,88 @@ ruleTester.run('shortcuts', rule, {
       errors: [
         explicitLengthComparisonError('a'),
       ],
+      output: 'if (a.length>0) {}',
     },
     {
       code: 'if (!a.length) {}',
       errors: [
         explicitLengthComparisonError('a'),
       ],
+      output: 'if (a.length===0) {}',
     },
     {
       code: 'if (a === true) {}',
       errors: [
         shortcutError('a', true),
       ],
+      output: 'if (a) {}',
+    },
+    {
+      code: 'if (a == true) {}',
+      errors: [
+        shortcutError('a', true),
+      ],
+      output: 'if (a) {}',
     },
     {
       code: 'if (a === false) {}',
       errors: [
         shortcutError('a', false),
       ],
+      output: 'if (!a) {}',
+    },
+    {
+      code: 'if (a == false) {}',
+      errors: [
+        shortcutError('a', false),
+      ],
+      output: 'if (!a) {}',
     },
     {
       code: 'if (a !== false) {}',
       errors: [
         shortcutError('a', false),
       ],
+      output: 'if (a) {}',
+    },
+    {
+      code: 'if (a != false) {}',
+      errors: [
+        shortcutError('a', false),
+      ],
+      output: 'if (a) {}',
     },
     {
       code: 'if (a !== true) {}',
       errors: [
         shortcutError('a', true),
       ],
+      output: 'if (!a) {}',
+    },
+    {
+      code: 'if (a != true) {}',
+      errors: [
+        shortcutError('a', true),
+      ],
+      output: 'if (!a) {}',
     },
   ],
 
   valid: [
+    'if (a.length == 0) {}',
     'if (a.length === 0) {}',
+    'if (a.length == 1) {}',
     'if (a.length === 1) {}',
     'if (a.length > 0) {}',
     'if (a) {}',
     'if (!a) {}',
+    'if (a == "") {}',
     'if (a === "") {}',
+    'if (a == 0) {}',
     'if (a === 0) {}',
+    'if (a != 0) {}',
     'if (a !== 0) {}',
     'if (a > 3) {}',
+    'if (a < 3) {}',
   ],
 });
